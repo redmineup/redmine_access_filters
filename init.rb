45ba0350d29f879c6d56a983dde3b5e8e3363872
Niklaus Giger <niklaus.giger@member.fsf.org>
2020-04-02 15:51:03 +0200

    Updated version to 0.0.2

--- init.rb
+++ init.rb
@@ -2,7 +2,7 @@ Redmine::Plugin.register :redmine_access_filters do
   name 'Redmine Access Filters plugin'
   author 'Redmine CRM'
   description 'Allows setting access filters for API and regular browser access per user'
-  version '0.0.1'
+  version '0.0.2'
   url 'http://redminecrm.com'
   author_url 'mailto:support@redminecrm.com'
   menu :admin_menu, :access_filters,
