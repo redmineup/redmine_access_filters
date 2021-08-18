1a10637e5a3e6721e074e2292ae1d7c8121ea762
Niklaus Giger <niklaus.giger@member.fsf.org>
2021-08-18 18:07:29 +0200

    [22953] Push version to 1.0.1. Reorder_handle does not work, but does not crash neither

--- init.rb
+++ init.rb
@@ -2,7 +2,7 @@ Redmine::Plugin.register :redmine_access_filters do
   name 'Redmine Access Filters plugin'
   author 'Redmine CRM'
   description 'Allows setting access filters for API and regular browser access per user'
-  version '1.0.0'
+  version '1.0.1'
   url 'https://github.com/ngiger/redmine_access_filters'
   author_url 'mailto:niklaus.giger@member.fsf.org'
   menu :admin_menu, :access_filters,
